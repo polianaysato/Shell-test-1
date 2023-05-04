cccfe0cc4b4717dc6929087e89400eaf86437a4a
poliana.sato <poliana.sato@vdx.tv>
2023-05-04 11:18:30 +0100

    Changes in lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,7 @@
-import numpy as np
-import scipy.stats as si
-
+import pandas as pd
 
 def lambda_handler(event, context):
-    _normal_distribution(0, 1)
-
-
-def _normal_distribution(value):
-    normal_dist = si.norm.cdf(value, 0.0, 1.0)
-    print(normal_dist, 'NORMAL DISTRIBUTION')
-    return normal_dist
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1.1')
